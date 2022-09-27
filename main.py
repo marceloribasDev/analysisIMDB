990e4bd8f62b8dbcaa7e5d03f30906cbe7b50df3
Marcelo Ribas <marcelo.ribas@icloud.com>
2022-09-27 22:12:05 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Imports
+import re
+import time
+import sqlite3
+import pycountry
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import cm
+from sklearn.feature_extraction.text import CountVectorizer
+import warnings
+warnings.filterwarnings("ignore")
+sns.set_theme(style="whitegrid")
