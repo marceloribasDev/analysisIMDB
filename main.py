9e112b064d83f94ca4afb224d5c11b73099dbd9f
Marcelo Ribas <marcelo.ribas@icloud.com>
2022-09-28 16:14:10 +0100

    Added test of connection of database

--- main.py
+++ main.py
@@ -37,7 +37,9 @@ warnings.filterwarnings("ignore")
 # Configurado o grid dos graficos
 sns.set_theme(style="whitegrid")
 
-# ================================================================================
+# =========================================================================
+# Preparing the environment and connection tests with the db
+# =========================================================================
 
 # Connect db
 conn = sqlite3.connect("imdb.db")
@@ -47,3 +49,16 @@ tabelas = pd.read_sql_query(
     "SELECT NAME AS 'Table_Name' FROM sqlite_master WHERE type = 'table'", conn)
 
 tabelas.head()
+
+
+# Vamos converter o dataframe em uma lista
+tabelas = tabelas["Table_Name"].values.tolist()
+
+# Vamos percorrer a lista de tabelas no banco de dados e extrair o esquema de cada uma
+for tabela in tabelas:
+    consulta = "PRAGMA TABLE_INFO({})".format(tabela)
+    resultado = pd.read_sql_query(consulta, conn)
+    print("Esquema da tabela:", tabela)
+    display(resultado)
+    print("-"*100)
+    print("\n")
